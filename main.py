ec36e9264a9269911c632f3ae8aec68ad05aa059
yui019 <yui36189@gmail.com>
2025-02-20 20:06:23 +0100

    Formatting
    no idea why vs code didn't do this for the earlier commits...

--- main.py
+++ main.py
@@ -4,24 +4,25 @@ import random
 
 STATE_SIZE = 2
 
+
 def build_model(names):
     model = {}
     for name in names:
         for i in range(STATE_SIZE, len(name)):
             current_char = name[i]
-            previous_chars = name[i-STATE_SIZE:i]
+            previous_chars = name[i - STATE_SIZE : i]
 
             # add entry
             if previous_chars in model:
                 model[previous_chars].append(current_char)
             else:
                 model[previous_chars] = [current_char]
-            
+
             # also add entry for the end-of-name symbol (#)
-            if i == len(name)-1:
-                previous_chars = name[i-STATE_SIZE+1:i+1]
+            if i == len(name) - 1:
+                previous_chars = name[i - STATE_SIZE + 1 : i + 1]
                 current_char = "#"
-                
+
                 if previous_chars in model:
                     model[previous_chars].append(current_char)
                 else:
@@ -29,20 +30,20 @@ def build_model(names):
 
     return model
 
+
 def generate(model, start):
     result = start
 
-    while result[len(result)-1] != "#":
+    while result[len(result) - 1] != "#":
         previous_chars = result[-STATE_SIZE:]
-        
+
         if previous_chars in model:
             result += random.choice(model[previous_chars])
         else:
             result += "#"
-    
+
     # Remove last char (which is always #)
-    return result[:len(result)-1]
-        
+    return result[: len(result) - 1]
 
 
 print("Loading data...")
@@ -50,11 +51,11 @@ print("Loading data...")
 names = []
 with open("us-names-by-year.csv", "r") as file:
     reader = csv.reader(file)
-    next(reader) # skip first row
+    next(reader)  # skip first row
     for row in reader:
         names.append(row[1])
 
-with open('names.json', 'w') as f:
+with open("names.json", "w") as f:
     json.dump(names, f, ensure_ascii=False, indent=4)
 
 print("Done!")
@@ -64,7 +65,7 @@ print("Building model...")
 
 model = build_model(names)
 
-with open('model.json', 'w') as f:
+with open("model.json", "w") as f:
     json.dump(model, f, ensure_ascii=False, indent=4)
 
 print("Done!")
